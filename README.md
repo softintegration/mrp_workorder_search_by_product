56337df87ffe1efad8874ddc72305d216771e3ce
Mossaab BOUDCHICHA <boudchicha23@hotmail.fr>
2023-07-15 11:08:06 +0100

    Initial commit

---
+++ README.md
@@ -0,0 +1,6 @@
+Manufacturing Work Order search with product
+--------------------------------------------
+This module add the research by product in the workorder search view.
+
+
+
---
+++ __manifest__.py
@@ -0,0 +1,21 @@
+# -*- coding: utf-8 -*- 
+
+
+{
+ 'name': 'Manufacturing Work Order search with product',
+ 'author': 'Soft-integration',
+ 'application': False,
+ 'installable': True,
+ 'auto_install': False,
+ 'qweb': [],
+ 'description': False,
+ 'images': [],
+ 'version': '1.0.1',
+ 'category': 'Manufacturing/Manufacturing',
+ 'demo': [],
+ 'depends': ['mrp'],
+ 'data': [
+    'views/mrp_workorder_views.xml'
+    ],
+ 'license': 'LGPL-3',
+ }
